06c925754a85155822da8a334ff820d032af0ffa
uo-zakana <sakana.uo1213@gmail.com>
2026-07-09 16:57:17 +0900

    デプロイ失敗のため、puma.rbファイルを修正

--- config/puma.rb
+++ config/puma.rb
@@ -11,34 +11,39 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-rails_env = ENV.fetch("RAILS_ENV") { "development" }
-
-if rails_env == "production"
-  # If you are running more than 1 thread per process, the workers count
-  # should be equal to the number of processors (CPU cores) in production.
-  #
-  # It defaults to 1 because it's impossible to reliably detect how many
-  # CPU cores are available. Make sure to set the `WEB_CONCURRENCY` environment
-  # variable to match the number of processors.
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { 1 })
-  if worker_count > 1
-    workers worker_count
-  else
-    preload_app!
-  end
-end
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
-
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+# Render が提供する PORT 環境変数を使用
+port_number = ENV.fetch("PORT") { 3000 }
+port port_number
+
+# すべてのネットワークインターフェースでリクエストを受け付ける
+bind "tcp://0.0.0.0:#{port_number}"
 
 # Specifies the `environment` that Puma will run in.
+rails_env = ENV.fetch("RAILS_ENV") { "development" }
 environment rails_env
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
+# ワーカー数の設定（本番環境のみ）
+if rails_env == "production"
+  # Render の無料プランではワーカー数を 1 に設定することを推奨
+  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { 1 })
+  workers worker_count
+
+  # プリロード（メモリ効率が良くなる）
+  preload_app!
+
+  # フォーク前の処理
+  on_worker_boot do
+    ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+  end
+end
+
+# Specifies the `worker_timeout` threshold that Puma will use to wait before
+# terminating a worker in development environments.
+worker_timeout 3600 if rails_env == "development"
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
